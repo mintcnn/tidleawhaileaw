26f02c6d5b97fe655adea58664f2470c299f901a
mintcnn <mintchnrt@gmail.com>
2021-04-11 16:03:12 +0700

    add number of covid situation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
 
-function App() {
+import Clock from "react-live-clock";
+import moment from "moment";
+import 'moment-timezone';
+import WebFont from 'webfontloader';
+
+import Chat from './chat';
+// import Map from './map'
+import './App.scss';
+import { getSituation, getTraffy } from './service/service';
+
+
+const  App = () => {
+  const dateFormat = "ddd DD MMM";
+  var dateNow = moment().format(dateFormat);
+  const [dataSituation, setDataSituation] = useState({});
+  const [dataTraffy, setDataTraffy] = useState({})
+
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ['Kanit', 'sans-serif']
+      }
+    })
+    situation()
+    // traffy()
+  }, [])
+
+  const situation = async () => {
+    try {
+      const res = await getSituation();
+      console.log(res.data);
+      setDataSituation(res.data);
+
+    } catch (error) {
+      throw error
+    }
+  }
+
+  const traffy = async () => {
+    try {
+      const res = await getTraffy()
+      console.log(res.data);
+      setDataTraffy(res.data)
+    } catch (error) {
+      throw error
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="containner">
+      <h1 className="center font-color">Tid Leaw Hai Leaw</h1>
+      <div className="center box-time">
+        <Clock format={"HH:mm:ss"} ticking={true} timezone={"Asia/Bangkok"} />
+        
+        <div className="font-date">{dateNow}</div>
+      </div>
+        <div className="flex-row">
+          <div className="box new">
+              ผู้ป่วยรายใหม่วันนี้ : {dataSituation.NewConfirmed}
+          </div>
+          <div className="box all">
+              ผู้ป่วยยืนยันสะสม : {dataSituation.Confirmed}
+          </div>
+        </div>
+        <div className="box recovered">
+          <div>
+            หายป่วยแล้ว : {dataSituation.Recovered}
+          </div>
+          <div>
+            หายป่วยเพิ่มขึ้น : {dataSituation.NewRecovered}
+          </div>
+          <div>
+            กำลังรักษาอยู่ : {dataSituation.Hospitalized}
+          </div>
+          <div>
+            รักษาเพิ่มขึ้น : {dataSituation.NewHospitalized}
+          </div>
+        </div>
+        <div className="box death">
+          <div>
+            เสียชีวิต : {dataSituation.Deaths}
+          </div>
+          <div>
+            เสียชีวิตเพิ่ม : {dataSituation.NewDeaths}
+          </div>
+        </div>
+
+     
+        
+      
+      {/* <div className="flex-center">
+        <Map />
+      </div>
+      <hr /> */}
+      <div className="center font-color">
+        <h3>Tid Leaw Hai Leaw Chatbot</h3>
+        <h4>Let's start chatting</h4>
+      </div>
+      <Chat/>
     </div>
   );
 }
